c9bc40a276ab5b2468d08cf50af0c56fc7cc4b0f
Next Page 01 <pilotymc@gmail.com>
2021-09-08 00:57:17 +0900

    Update and rename a.py to checkuser.py

---
+++ Lambda/checkuser.py
@@ -0,0 +1,39 @@
+import sys
+import logging
+import rds_config
+import pymysql
+import json
+import datetime
+#rds settings
+rds_host  = "haniumtrafficlight.cbk0vfgjfbad.us-east-2.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def json_default(value): 
+    if isinstance(value, datetime.date): 
+        return value.strftime('%Y-%m-%d') 
+        raise TypeError('not JSON serializable')
+
+
+try:
+    conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+def lambda_handler(event, context):
+    with conn.cursor() as cur:
+        data = json.dumps(event)
+        data1= json.loads(data)['body']
+        a   = str(data1["user_id"])
+        
+        cur.execute(f"""select * from logindb where user_id = '{a}'""")
+        datas= json.dumps(cur.fetchall(),default=json_default)
+
+    return datas
