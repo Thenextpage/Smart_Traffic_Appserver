e85930f929133f8c6d2f27977def7fb8207a51b2
Next Page 01 <pilotymc@gmail.com>
2021-11-12 00:42:21 +0900

    Create uploadimageobject.py

---
+++ Lambda/uploadimageobject.py
@@ -0,0 +1,37 @@
+import sys
+import logging
+import rds_config
+import pymysql
+import json
+import datetime
+#rds settings
+rds_host  = "haniumtrafficlight.cbk0vfgjfbad.us-east-2.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+
+try:
+    conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+def lambda_handler(event, context):
+    with conn.cursor() as cur:
+        b   = float(event["latitude"])
+        c   = float(event["longitude"])
+        d   = str(event["objectname"])
+        
+        try:
+            cur.execute(f"""insert into `trafficappdb`.`image_db` (`latitude`,`longitude`, `objectname`,`time`) values({b},{c},"{d}",DATE_ADD(NOW(),INTERVAL 9 hour)) """)
+            conn.commit()
+        except Exception as e:
+            return e
+    return
